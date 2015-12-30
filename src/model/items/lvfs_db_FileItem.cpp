0c9d2120c2a6fe99634bd6867dd449be88213ac6
Dmitriy Vilkov <dav.daemon@gmail.com>
2015-12-30 20:25:05 +0300

    Show location of the file instead of its name

--- src/model/items/lvfs_db_FileItem.cpp
+++ src/model/items/lvfs_db_FileItem.cpp
@@ -39,7 +39,7 @@ FileItem::FileItem(const EntityValue &value, const Interface::Holder &file, Item
     {
         m_name = toUnicode(m_file->as<IEntry>()->title());
         m_icon.addFile(toUnicode(m_file->as<IEntry>()->type()->icon()->as<IEntry>()->location()), QSize(16, 16));
-        m_toolTip = toUnicode(m_file->as<IEntry>()->type()->name());
+        m_toolTip = toUnicode(m_file->as<IEntry>()->location());
 
         if (IDirectory *dir = m_file->as<IDirectory>())
             for (IDirectory::const_iterator i = dir->begin(), end = dir->end(); i != end; ++i)
